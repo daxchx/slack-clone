96483eb8242e320672aa56bb89a9be89dfd59143
daxchx <daxchx.v1@gmail.com>
2024-03-21 12:16:02 +0900

    firestoreにチャンネルデータを保存

---
+++ src/features/channel/channelAPI.ts
@@ -0,0 +1,38 @@
+import { getFirestore, query, collection, addDoc, onSnapshot, Timestamp } from 'firebase/firestore'
+import { firebaseApp } from '../../firebase/firebaseConfig'
+import { Channel, ChannelRef } from '../../type/Channel'
+
+const db = getFirestore(firebaseApp)
+
+export const subscribeChannels = (onChannelsUpdated: (channels: ChannelRef[]) => void) => {
+  const q = query(collection(db, 'channels'))
+
+  return onSnapshot(
+    q,
+    (querySnapshot) => {
+      const channelRefs: ChannelRef[] = []
+      querySnapshot.forEach((doc) => {
+        channelRefs.push({
+          id: doc.id,
+          channel: doc.data() as Channel,
+        })
+      })
+      onChannelsUpdated(channelRefs)
+    },
+    (error) => {
+      console.error('Failed to subscribe messages:', error)
+    }
+  )
+}
+
+export const postChannel = async (channel: Channel) => {
+  await addDoc(collection(db, 'channels'), channel)
+}
+
+export const createChannel = (name: string): Channel => {
+  const timestamp = Timestamp.fromDate(new Date())
+  return {
+    name: name,
+    create_at: timestamp,
+  }
+}
