6cb3f2fb559ec9ed552ac355e88575b719f22cc3
daxchx <daxchx.v1@gmail.com>
2024-03-21 11:08:38 +0900

    認証機能を追加

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { signInWithGoogle } from '../features/auth/Auth'
 
 const Login = () => {
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithGoogle()
+      const login_user = result.user
+      console.log(login_user)
+    } catch (error) {
+      console.error('Login failed:', error)
+    }
+  }
+
   return (
     <div className="fixed flex inset-0 items-center justify-center bg-gray-500">
       <div className="w-full max-w-xs">
@@ -9,7 +20,9 @@ const Login = () => {
             <h1 className="text-3xl text-center text-gray-700 mt-4">Slackにログイン</h1>
           </div>
           <div className="flex items-center justify-center">
-            <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">ログイン</button>
+            <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={loginWithGoogle}>
+              ログイン
+            </button>
           </div>
         </div>
       </div>
